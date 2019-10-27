f8977e9453d024c97507bddecc5b2d52a147c7df
sprogslegs <sprogslegs@users.noreply.github.com>
2019-10-27 23:04:28 +0000

    Loading works, scoring remaining

--- DojoTemplateConsoleApp/BowlingGame.cs
+++ DojoTemplateConsoleApp/BowlingGame.cs
@@ -2,9 +2,80 @@
 using System.Collections.Generic;
 using System.Text;
 
+/// <summary>
+/// http://codingdojo.org/kata/Bowling/
+/// </summary>
 namespace DojoTemplateConsoleApp
 {
-    class BowlingGame
+    public class BowlingGame
     {
+        public IEnumerable<int> LoadScores(string inputScores)
+        {
+            var scores = new List<int>();
+            var trimmedScores = inputScores.Replace(" ", String.Empty);
+            for (int i = 0; i < trimmedScores.Length; i++)
+            {
+                if (trimmedScores[i] == 'X')
+                {
+                    scores.Add(10);
+                }
+                else if (trimmedScores[i] == '/')
+                {
+                    var remainder = int.Parse(trimmedScores[i - 1].ToString());
+                    scores.Add(10 - remainder);
+                }
+                else if (trimmedScores[i] == '-')
+                {
+                    scores.Add(0);
+                }
+                else
+                {
+                    var score = int.Parse(trimmedScores[i].ToString());
+                    scores.Add(score);
+                }
+            }
+            
+            return scores;
+        }
+
+        public int CalculateTotalScore(List<int> scores)
+        {
+            var totalScore = 0;
+
+            for (int i = 0; i < scores.Count; i++)
+            {
+                if (scores[i] == 10)
+                {
+                    totalScore += 10 + scores[i + 1] + scores[i + 2];
+
+                    if (i == (scores.Count - 3))
+                    {
+                        break;
+                    }
+                }
+
+                else if ((i < (scores.Count - 2)) && (scores[i] + scores[i + 1]) == 10)
+                {
+                    totalScore += 10 + scores[i + 2];
+
+                    if (i == (scores.Count - 3))
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        i ++;
+                    }
+                }
+
+                else
+                {
+                    totalScore += scores[i] + scores[i + 1];
+                    i++;
+                }
+            }
+
+            return totalScore;
+        }
     }
 }
--- DojoTemplateTestProject/DojoTemplateTestProject/BowlingTests.cs
+++ DojoTemplateTestProject/DojoTemplateTestProject/BowlingTests.cs
@@ -1,13 +1,69 @@
+using DojoTemplateConsoleApp;
 using System;
+using System.Collections.Generic;
 using Xunit;
 
 namespace DojoTemplateTestProject
 {
-    public class UnitTest1
+    public class BowlingTests
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("X X X X X X X X X X X X", "101010101010101010101010")]
+        [InlineData("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", "90909090909090909090")]
+        [InlineData("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", "555555555555555555555")]
+        [InlineData("X 45 7/ 3- X X 5/ 22 X 4/8", "104573301010552210468")]
+        [InlineData("9- 52 2- 7/ -- 16 8/ 11 43 -7", "90522073001682114307")]
+        [InlineData("9- 52 2- 71 -- 16 80 11 43 -7", "90522071001680114307")]
+        public void TestScoresLoaded(string inputScores, string expectedScoresString)
         {
+            // ARRANGE
+            var bowlingGame = new BowlingGame();
+
+
+            // ACT
+            var actualScores = bowlingGame.LoadScores(inputScores);
+            string actualScoresString = string.Empty;
+            foreach (var score in actualScores)
+            {
+                var scoreString = score.ToString();
+                actualScoresString += scoreString;
+            }
+
+            // ASSERT
+            Assert.Equal(expectedScoresString, actualScoresString);
+        }
+
+        [Theory]
+        [InlineData("101010101010101010101010", 300)]
+        [InlineData("90909090909090909090", 90)]
+        [InlineData("555555555555555555555", 150)]
+        [InlineData("104573301010552210468", 143)]
+        [InlineData("90522071001680114307", 57)]
+        public void TestCalculateTotalScore(string scores, int totalScore)
+        {
+            // ARRANGE
+            var bowlingGame = new BowlingGame();
+            var scoresList = new List<int>();
+            for (int i = 0; i < scores.Length; i++)
+            {
+                if (i < scores.Length - 1 && (Equals((scores[i].ToString() + scores[i + 1].ToString()), "10")))
+                {
+                    scoresList.Add(10);
+                    i++;
+                }
+
+                else
+                {
+                    var score = int.Parse(scores[i].ToString());
+                    scoresList.Add(score);
+                }
+            }
+                
+            // ACT
+            var actualScore = bowlingGame.CalculateTotalScore(scoresList);
+
+            // ASSERT
+            Assert.Equal(totalScore, actualScore);
 
         }
     }

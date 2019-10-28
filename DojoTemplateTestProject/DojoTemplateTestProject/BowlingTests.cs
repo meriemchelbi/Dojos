db9bb69cb5b326df26b64c8ed35fda737f4e22bd
sprogslegs <sprogslegs@users.noreply.github.com>
2019-10-28 11:48:32 +0000

    Added Cedric's implementation

--- DojoTemplateTestProject/DojoTemplateTestProject/BowlingTests.cs
+++ DojoTemplateTestProject/DojoTemplateTestProject/BowlingTests.cs
@@ -12,7 +12,7 @@ namespace DojoTemplateTestProject
         [InlineData("9- 9- 9- 9- 9- 9- 9- 9- 9- 9-", "90909090909090909090", 90)]
         [InlineData("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5", "555555555555555555555", 150)]
         [InlineData("X 45 7/ 3- X X 5/ 22 X 4/8", "104573301010552210468", 143)]
-        [InlineData("9- 52 2- 71 -- 16 80 11 43 -7", "90522071001680114307",57)]
+        [InlineData("9- 52 2- 71 -- 16 80 11 43 -7", "90522071001680114307", 57)]
         public void TestScoresLoadedAndCalculated(string inputScores, string expectedScoresString, int totalScore)
         {
             // ARRANGE

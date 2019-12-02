515813c24311272931608c3e34ff3d151dbe77f5
sprogslegs <meriemchelbi@outlook.com>
2019-12-02 22:58:17 +0000

    Now with accidentally omitted files

---
+++ DojoTemplateConsoleApp/ModuleMassParser.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DojoTemplateConsoleApp
+{
+    public class ModuleMassParser
+    {
+        public List<int> ModuleMasses
+        {
+            get
+            { 
+                if (_moduleMasses is null) 
+                    _moduleMasses = ParseModuleMass();
+                return _moduleMasses;
+            }
+            private set {}
+        }
+
+        private List<int> _moduleMasses;
+        
+        public List<int> ParseModuleMass()
+        {
+            var moduleMasses = new List<int>();
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\merie\Documents\GitHub\Dojos\DojoTemplateConsoleApp\ModuleMasses.txt");
+            foreach (string line in lines)
+            {
+                var moduleMass = int.Parse(line);
+                moduleMasses.Add(moduleMass);
+            }
+
+            return moduleMasses;
+        
+        }
+        
+    }
+}
---
+++ DojoTemplateTestProject/DojoTemplateTestProject/FuelRequirementsTests.cs
@@ -0,0 +1,46 @@
+using System;
+using Xunit;
+using DojoTemplateConsoleApp;
+
+namespace DojoTemplateTestProject
+{
+    public class FuelRequirementsTests
+    {
+        [Theory]
+        [InlineData(51585, 0)]
+        [InlineData(103969, 16)]
+        [InlineData(72867, 56)]
+        [InlineData(102481, 90)]
+        [InlineData(126543, 99)]
+        public void ModuleMassParsesStringToIntMass(int expectedMass, int elementIndex)
+        {
+            var moduleMassParser = new ModuleMassParser();
+            
+            Assert.Equal(expectedMass, moduleMassParser.ModuleMasses[elementIndex]);
+        }
+                
+        
+        [Theory]
+        [InlineData(14, 2)]
+        [InlineData(1969, 966)]
+        [InlineData(100756, 50346)]
+        public void CalculateModuleFuelMassShouldReturnCorrectMass(int moduleMass, int expectedFuelMass)
+        {
+            var fuelMassCalculator = new FuelMassCalculator();
+            var actualModuleMass = fuelMassCalculator.CalculateModuleFuelMass(moduleMass);
+
+            Assert.Equal(expectedFuelMass, actualModuleMass);
+        }
+
+
+        //[Fact]
+        //public void CalculateTotalFuelRequirementsShouldReturnCorrectTotal()
+        //{
+        //    var fuelMassCalculator = new FuelMassCalculator();
+        //    var actualTotalFuelMass = fuelMassCalculator.CalculateTotalFuelRequirements();
+
+        //    Assert.Equal(3384232, actualTotalFuelMass);
+        }
+        
+    }
+}

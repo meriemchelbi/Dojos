315927295625c7f295b72456b24b6e10dd9880a6
sprogslegs <meriemchelbi@outlook.com>
2019-12-08 20:58:09 +0000

    SpaceImageFormat added

---
+++ DojoTemplateConsoleApp/SpaceImageFormat.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DojoTemplateConsoleApp
+{
+    public class SpaceImageFormat
+    {
+        public SpaceImageFormat()
+        {
+            Layers = new Dictionary<int, List<string>>();
+        }
+        public Dictionary<int, List<string>> Layers { get; set; }
+
+        public void IsolateLayers(string imagePixels, int layerWidth, int layerHeight)
+        {
+            var noOfLayers = imagePixels.Length / (layerHeight * layerWidth);
+
+            for (int i = 1; i <= noOfLayers; i++)
+            {
+                var layer = new List<string>();
+
+                for (int lineNo = 0; lineNo < layerHeight; lineNo++)
+                {
+                    var stringBuilder = new StringBuilder();
+
+                    for (int pixel = 0; pixel < layerWidth; pixel++) // create line in layer
+                    {
+                        var selectedPixel = imagePixels[pixel];
+                        stringBuilder.Append(selectedPixel);
+                    }
+
+                    var line = stringBuilder.ToString();
+                    layer.Add(line);
+                    imagePixels = imagePixels.Remove(0, 3);
+                }
+
+                Layers.Add(i, layer);
+            }
+        }
+
+        public object CountInstancesOfDigit(List<string> layer, int digit)
+        {
+            var stringDigit = digit.ToString();
+            var digitCount = 0;
+
+            foreach (var line in layer)
+            {
+                digitCount += line.Count(c => digit.Equals(c));
+            }
+
+            return digitCount;
+        }
+
+    }
+
+    public class Layer
+    {
+        public int Height { get; set; }
+
+        public int Width { get; set; }
+
+        public List<string> Lines { get; set; }
+    }
+}
---
+++ DojoTemplateTestProject/DojoTemplateTestProject/SpaceImageFormatTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using NSubstitute;
+using DojoTemplateConsoleApp;
+
+namespace DojoTemplateTestProject
+{
+    public class SpaceImageFormatTests
+    {
+        [Fact]
+        public void IsolateLayersCreatesLayers()
+        {
+            var imagePixels = "123456789012";
+            var layerWidth = 3;
+            var layerHeight = 2;
+            var substitute = Substitute.For<SpaceImageFormat>();
+            var expectedLayers = new Dictionary<int, List<string>>()
+            {
+                { 1, new List<string>(){ "123", "456" } },
+                { 2 ,new List<string>(){ "789", "012" } }
+            };
+
+            substitute.IsolateLayers(imagePixels, layerWidth, layerHeight);
+
+            substitute.Layers.Should().BeEquivalentTo(expectedLayers);
+        }
+
+        [Theory]
+        [InlineData("1224", "5241", "9444", 4, 5)]
+        [InlineData("15554", "52415", "59444", 5, 6)]
+        public void CountInstancesOfDigitInLayerReturnsCorrectCount(string line1, string line2, string line3, int digit, int count)
+        {
+            var layer = new List<string>() { line1, line2, line3 };
+            var sut = new SpaceImageFormat();
+
+            var actualCount = sut.CountInstancesOfDigit(layer, digit);
+
+            actualCount.Should().Equals(count);
+        }
+
+
+        [Fact]
+        public void FindLayerWithFewestDigitInstancesReturnsCorrectLayer()
+        {
+
+        }
+    }
+}

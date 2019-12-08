2b82d5b06124f1d271a05a8d8827ab3501f95790
sprogslegs <meriemchelbi@outlook.com>
2019-12-08 18:57:10 +0000

    Day 4 part 1 working, part 2 not working
    Current answer 1324 rejected as too high

---
+++ DojoTemplateConsoleApp/SecureContainer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DojoTemplateConsoleApp
+{
+    public class SecureContainer
+    {
+
+        public bool HasOnlyIncreasingDigits(int password)
+        {
+            var passwordString = password.ToString();
+            var hasOnlyIncreasingDigits = true;
+
+            for (int i = 0; i < passwordString.Length - 1; i++)
+            {
+                if (passwordString[i] > passwordString[i + 1])
+                {
+                    hasOnlyIncreasingDigits = false;
+                    break;
+                }
+            }
+
+            return hasOnlyIncreasingDigits;
+        }
+
+        public int FindNumberOfMatches(int lowerBound, int higherBound)
+        {
+            var matches = 0;
+            int value = lowerBound;
+
+            while (value >= lowerBound && value <= higherBound)
+            {
+                if (HasTwoAdjacentMatchingDigits(value) && HasOnlyIncreasingDigits(value))
+                {
+                    matches++;
+                }
+
+                value++;
+            }
+
+            return matches;
+        }
+
+        public bool HasTwoAdjacentMatchingDigits(int password)
+        {
+            var passwordString = password.ToString();
+            var hasTwoAdjacentMatchingDigits = false;
+            var tempValue = false;
+
+            for (int i = 0; i < passwordString.Length - 1; i++)
+            {
+                if (passwordString[i].Equals(passwordString[i + 1]) && !hasTwoAdjacentMatchingDigits)
+                {
+                    hasTwoAdjacentMatchingDigits = true;
+                    tempValue = true;
+                }
+                else if (passwordString[i].Equals(passwordString[i + 1]) && tempValue)
+                {
+                    hasTwoAdjacentMatchingDigits = false;
+                }
+                else
+                {
+                    tempValue = false;
+                }
+            }
+
+            return hasTwoAdjacentMatchingDigits;
+        }
+    }
+}
---
+++ DojoTemplateTestProject/DojoTemplateTestProject/SecureContainerTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using DojoTemplateConsoleApp;
+
+namespace DojoTemplateTestProject
+{
+    public class SecureContainerTests
+    {
+        [Theory]
+        [InlineData(156218, 652527, 1324)]
+        public void DisplayNumberOfMatchingPasswords(int lowerBound, int upperBound, int expectedResult)
+        {
+            var secureContainer = new SecureContainer();
+            var result = secureContainer.FindNumberOfMatches(lowerBound, upperBound);
+
+            Assert.Equal(expectedResult, result);
+        }
+        
+        [Theory]
+        [InlineData(123456, false)]
+        [InlineData(125526, true)]
+        [InlineData(125556, false)]
+        [InlineData(1222556, true)]
+        [InlineData(112233, true)]
+        [InlineData(112345, true)]
+        [InlineData(123444, false)]
+        [InlineData(11123444, false)]
+        [InlineData(111122, true)]
+        public void PasswordContainsTwoAdjacentDigits(int password, bool containsAdjacentDoubleDigits)
+        {
+            var secureContainer = new SecureContainer();
+            var result = secureContainer.HasTwoAdjacentMatchingDigits(password);
+
+            result.Should().Be(containsAdjacentDoubleDigits);
+        }
+
+        [Theory]
+        [InlineData(123456, true)]
+        [InlineData(125726, false)]
+        [InlineData(405789, false)]
+        [InlineData(457893, false)]
+        [InlineData(124078, false)]
+        public void PasswordHasOnlyIncreasingDigits(int password, bool hasDecreasingDigits)
+        {
+            var secureContainer = new SecureContainer();
+            var result = secureContainer.HasOnlyIncreasingDigits(password);
+
+            result.Should().Be(hasDecreasingDigits);
+        }
+
+    }
+}

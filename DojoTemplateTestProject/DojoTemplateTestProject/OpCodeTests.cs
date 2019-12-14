c55e01186b4cfcd6842a485bcaadb27566a92ca9
sprogslegs <meriemchelbi@outlook.com>
2019-12-14 12:04:02 +0000

    Merge conflicts resolved

--- DojoTemplateTestProject/DojoTemplateTestProject/OpCodeTests.cs
+++ DojoTemplateTestProject/DojoTemplateTestProject/OpCodeTests.cs
@@ -12,7 +12,7 @@ namespace DojoTemplateTestProject
     public class OpCodeTests
     {
         [Theory]
-        [InlineData(0, 1)]
+        [InlineData(12, 1)]
         [InlineData(4, 10)]
         [InlineData(1, 20)]
         public void OpCodeParserParsesToList(int opCode, int expectedElementIndex)
@@ -24,20 +24,19 @@ namespace DojoTemplateTestProject
             Assert.Equal(opCode, list[expectedElementIndex]);
         }
                 
-        // TODO add test for out of range
-        [Fact]
-        public void OpCodeExecutorInsertsExpectedResultInCorrectPosition()
+        [Theory]
+        [InlineData("33,4,6,5,1,0,3,5,2,7,3,4,99,2,1,7,3", "33,4,6,5,25,38,3,5,2,7,3,4,99,2,1,7,3")]
+        [InlineData("2,3,0,3,99", "2,3,0,6,99")]
+        [InlineData("1,0,0,0,99", "2,0,0,0,99")]
+        [InlineData("2,4,4,5,99,0", "2,4,4,5,99,9801")]
+        [InlineData("1,1,1,4,99,5,6,0,99", "30,1,1,4,2,5,6,0,99")]
+        public void OpCodeExecutorInsertsExpectedResultInCorrectPosition(string sourceCode, string expectedOutput)
         {
             var substitute = Substitute.For<OpCodeOperations>();
             substitute.Input = sourceCode.Split(',').Select(int.Parse).ToArray();
             substitute.ExecuteOpCode();
 
             substitute.Should().Equals(expectedOutput);
-
-            
-            Assert.Equal(38, substitute.OpCodes[5]);
-            Assert.Equal(25, substitute.OpCodes[4]);
-            Assert.Equal(5, substitute.OpCodes[3]);
         }
 
         [Fact]

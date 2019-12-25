cd97ca7e79ad4779c3487d7883aa345b8962e366
sprogslegs <meriemchelbi@outlook.com>
2019-12-25 12:15:03 +0000

    MoveWire implemented

--- DojoTemplateConsoleApp/CrossedWires/CrossedWires.cs
+++ DojoTemplateConsoleApp/CrossedWires/CrossedWires.cs
@@ -9,15 +9,53 @@ namespace DojoTemplateConsoleApp.CrossedWires
 {
     public class CrossedWires
     {
-        public List<string> wireOneDirections { get; set; }
-        public List<string> wireTwoDirections { get; set; }
+        public List<string> WireOneDirections { get; set; }
+        public List<string> WireTwoDirections { get; set; }
+        public object WireOnePath { get; set; }
+        public object WireTwoPath { get; set; }
 
         public void ParseInput()
         {
             var path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"CrossedWires\CrossedWiresInput.txt");
             var input = File.ReadAllLines(path);
-            wireOneDirections = input[0].Split(",").ToList();
-            wireTwoDirections = input[1].Split(",").ToList();
+            WireOneDirections = input[0].Split(",").ToList();
+            WireTwoDirections = input[1].Split(",").ToList();
+        }
+
+        public void LoadPaths()
+        {
+            var (x, y) = (0, 0);
+            WireOnePath = WireOneDirections.Select(i => (x, y) = MoveWire(i, (x, y))).ToList();
+
+            (x, y) = (0, 0);
+            WireTwoPath = WireTwoDirections.Select(i => (x, y) = MoveWire(i, (x, y))).ToList();
+        }
+
+        private (int, int) MoveWire(string instruction, (int, int) position)
+        {
+            var (x, y) = position;
+            var direction = instruction.Substring(0, 1);
+            var distance = int.Parse(instruction.Substring(1));
+
+            switch (direction)
+            {
+                case "U":
+                    y += distance;
+                    break;
+                case "D":
+                    y -= distance;
+                    break;
+                case "L":
+                    x -= distance;
+                    break;
+                case "R":
+                    x += distance;
+                    break;
+                default:
+                    break;
+            }
+
+            return (x, y);
         }
     }
 }
--- DojoTemplateTestProject/DojoTemplateTestProject/CrossedWiresTests.cs
+++ DojoTemplateTestProject/DojoTemplateTestProject/CrossedWiresTests.cs
@@ -15,15 +15,40 @@ namespace DojoTemplateTestProject
             var crossedWires = new CrossedWires();
             crossedWires.ParseInput();
 
-            crossedWires.wireOneDirections[0].Should().Be("R992");
-            crossedWires.wireTwoDirections[0].Should().Be("L998");
+            crossedWires.WireOneDirections[0].Should().Be("R992");
+            crossedWires.WireTwoDirections[0].Should().Be("L998");
 
         }
 
         [Fact]
         public void LoadPathsLoadsPathsToListOfCoordinates()
         {
+            var crossedWires = new CrossedWires()
+            {
+                WireOneDirections = new List<string> { "R8", "U5", "L5", "D3" },
+                WireTwoDirections = new List<string> { "U7", "R6", "D4", "L4" }
+            };
+            var expectedWireOnePath = new List<(int, int)> 
+            {
+                //(0, 0),
+                (8, 0),
+                (8, 5),
+                (3, 5),
+                (3, 2)
+            };
+            var expectedWireTwoPath = new List<(int, int)> 
+            { 
+                //(0, 0),
+                (0, 7),
+                (6, 7),
+                (6, 3),
+                (2, 3)
+            };
 
+            crossedWires.LoadPaths();
+
+            crossedWires.WireOnePath.Should().BeEquivalentTo(expectedWireOnePath);
+            crossedWires.WireTwoPath.Should().BeEquivalentTo(expectedWireTwoPath);
         }
 
         [Fact]

87af3cfaa78002869b0be3934a9ee9f112673d89
sprogslegs <meriemchelbi@outlook.com>
2020-08-16 17:45:24 +0100

    Cleaned up responsibilities

--- DojoTemplateTestProject/DojoTemplateTestProject/Skynet1.cs
+++ DojoTemplateTestProject/DojoTemplateTestProject/Skynet1.cs
@@ -7,18 +7,6 @@ namespace DojoTemplateTestProject
     public class Skynet1
     {
         [Fact]
-        public void SeveralPaths()
-        {
-            var nodes = 4;
-            var links = 4;
-            var gatweays = 4;
-
-            var result = KillTheVirus(nodes, links, gatweays);
-
-            Assert.Equal("2 3", result);
-        }
-        
-        [Fact]
         public void CanBuildGraph()
         {
             var standinput = new Queue<string>(new[]
@@ -65,6 +53,7 @@ namespace DojoTemplateTestProject
         {
             public List<(int,int)>  Links { get; set; }
             public List<Node> Nodes { get; set; }
+            public Virus Virus { get; set; }
 
             public Graph()
             {
@@ -92,6 +81,42 @@ namespace DojoTemplateTestProject
                 for (int i = 0; i < noOfNodes; i++)
                     Nodes.Add(new Node(i));
             }
+
+            // TODO Refactor BuildGraph to take in specific inputs
+            public void BuildGraph(string[] args)
+            {
+                string[] inputs;
+                
+                inputs = Console.ReadLine().Split(' ');
+
+                var noOfNodes = int.Parse(inputs[0]);
+                AddNodes(noOfNodes);
+                Console.Error.WriteLine($"No of nodes: {noOfNodes}");
+
+                var noOfLinks = int.Parse(inputs[1]);
+                Console.Error.WriteLine($"No of Links: {noOfLinks}");
+
+                var noOfGateways = int.Parse(inputs[2]);
+                Console.Error.WriteLine($"No of gateways: {noOfGateways}");
+
+                for (int i = 0; i < noOfLinks; i++)
+                {
+                    var link = Console.ReadLine().Split(' ');
+                    var node1 = int.Parse(link[0]);
+                    var node2 = int.Parse(link[1]);
+                    AddLink(node1, node2);
+                    Console.Error.WriteLine($"Nodes {node1} and node {node2} are linked");
+                }
+
+                for (int i = 0; i < noOfGateways; i++)
+                {
+                    var gatewayIndex = int.Parse(Console.ReadLine());
+                    Nodes[gatewayIndex].IsGateway = true;
+                    Console.Error.WriteLine($"Gateway index: {gatewayIndex}");
+                }
+
+                Virus = new Virus();
+            }
         }
 
         class Node
@@ -110,11 +135,28 @@ namespace DojoTemplateTestProject
         class Virus
         {
             public Node CurrentPosition { get; set; }
+            
+            public int GetCurrentPosition()
+            {
+                return int.Parse(Console.ReadLine());
+            }
         }
 
-        private string KillTheVirus(int nodes, int links, int gatweays)
+        // TODO introduce GetInput method that splits the input into component parts.
+        // Change BuildGraph to take these inputs, and KillTheVirus to take the graph as single argument
+        // That way, you can bypass GetInput in your tests and just new up a collection
+        private static string KillTheVirus(string[] args)
         {
-            throw new NotImplementedException();
+            var skynet = new Graph();
+            skynet.BuildGraph(args);
+
+            while (true)
+            {
+                var virusPosition = skynet.Virus.GetCurrentPosition();
+                skynet.Virus.CurrentPosition = skynet.Nodes[virusPosition];
+                Console.WriteLine("2 3");
+            }
         }
+
     }
 }

64843609d4cb027cc96eecd09da8069433691371
sprogslegs <meriemchelbi@outlook.com>
2020-10-11 17:11:49 +0100

    Create Staircase.cs

---
+++ DojoTemplateTestProject/DojoTemplateTestProject/Staircase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DojoTemplateTestProject
+{
+    public class Staircase
+    {
+        public void staircase(int n)
+        {
+            var matrix = new string[n, n];
+
+            for (int i = 0; i < n; i++)
+            {
+                var noOfStairs = i + 1;
+                var noOfSpaces = n - noOfStairs;
+
+                for (int sp = 0; sp < noOfSpaces; sp++)
+                    matrix[i, sp] = " ";
+
+                for (int h = noOfSpaces; h < n; h++)
+                    matrix[i, h] = "#";
+            }
+
+            for (int k = 0; k < n; k++)
+            {
+                for (int l = 0; l < n; l++)
+                    Console.Write(matrix[k, l]);
+                
+                Console.Write('\n');
+            }
+        }
+
+        [Fact]
+        public void Test()
+        {
+            staircase(5);
+        }
+    }
+
+}

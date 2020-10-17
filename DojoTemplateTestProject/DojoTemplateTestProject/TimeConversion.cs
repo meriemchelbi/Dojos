b1494058c06b2daaed4a379bc89bad25b175be6f
sprogslegs <meriemchelbi@outlook.com>
2020-10-17 20:44:11 +0100

    Create TimeConversion.cs

---
+++ DojoTemplateTestProject/DojoTemplateTestProject/TimeConversion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DojoTemplateTestProject
+{
+    public class TimeConversion
+    {
+        [Fact]
+        public void Test1()
+        {
+            var result = ConvertTime("07:05:45PM");
+
+            Assert.Equal("19:05:45", result);
+        }
+
+        private string ConvertTime(string input)
+        {
+            var convertedHours = string.Empty;
+            var hours = input.Substring(0, 2);
+
+            if (input.Substring(8) == "AM")
+                convertedHours = hours == "12" ? "00" : hours;
+
+            else
+                convertedHours = hours == "12"
+                                ? "12"
+                                : (int.Parse(hours) + 12).ToString();
+
+            var minutesSeconds = input.Substring(2, 6);
+
+            return convertedHours + minutesSeconds;
+        }
+    }
+}
